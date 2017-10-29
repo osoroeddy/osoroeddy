62e677228c9ac4973ffb573457277399449ee88b
osoroeddy <31865315+osoroeddy@users.noreply.github.com>
2017-10-29 13:38:49 -0500

    hangman changes

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,4 +1,16 @@
 defmodule Hangman do
+
+defstruct game_state: "initializing", letters: [], used: [], turns_left: 7, last_guess: []
+
+def new_game () do
+
+   spell_word = Dictionary.random_word()
+
+   game = %Hangman{}
+
+   new_map = Map.put( game, :word_to_spell, spell_word)
+   new_map
+  end
   @moduledoc """
   Documentation for Hangman.
   """
